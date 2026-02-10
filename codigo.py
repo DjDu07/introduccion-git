1465861e9d4929efe5ac21fdc52639529139472d
dijodiur <djdiazurday07@gmail.com>
2026-02-10 15:56:46 -0500

    Añadiendo la funcion suma al codigo

--- codigo.py
+++ codigo.py
@@ -1,3 +1,7 @@
+def operacion_suma(a, b):
+    if a > 0 and b > 0:
+        return a + b
+
 nombre = "Diego Joaquin"
 apellido = "Diaz"
 
@@ -5,3 +9,8 @@ saludo = "Hola "+ nombre + " " + apellido + " ¿Que tal tu dia?"
 
 print(saludo)
 
+a = int(input("Elige el primer numero para sumar: "))
+b = int(input("Elige el segundo numero a sumar: "))
+suma = operacion_suma(a,b)
+
+print("La suma de los dos numeros es: ", suma)
